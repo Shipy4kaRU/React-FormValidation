a23cbb15dd22a581a731f5813d197247fbcc07dd
Danil Shipy4ka <105577586+Shipy4kaRU@users.noreply.github.com>
2024-10-08 20:29:48 +0400

    input name validation

--- src/components/SomeInput.js
+++ src/components/SomeInput.js
@@ -1,12 +1,40 @@
+import { useState } from "react";
+
 const SomeInput = (props) => {
+  const [nameInput, setNameInput] = useState("");
+  const [click, setClick] = useState(false);
+
+  const isNameValid = nameInput.trim() !== "";
+  const isInputNotValid = click && !isNameValid;
+
+  const submitFormHandler = (e) => {
+    e.preventDefault();
+    if (isNameValid) console.log("SENDING");
+  };
+
+  const blurInputHandler = () => {
+    setClick(true);
+  };
+
+  const changeNameHandler = (e) => {
+    setNameInput(e.target.value);
+  };
+
   return (
-    <form>
-      <div className="form-control">
+    <form onSubmit={submitFormHandler}>
+      <div className={`form-control ${isInputNotValid ? "invalid input" : ""}`}>
         <label htmlFor="name">Введите Имя</label>
-        <input type="text" id="name" />
+        <input
+          type="text"
+          id="name"
+          onChange={changeNameHandler}
+          value={nameInput}
+          onBlur={blurInputHandler}
+        />
       </div>
+      {isInputNotValid ? <p className="error-text">Обязательное поле</p> : ""}
       <div className="form-actions">
-        <button>Отправить</button>
+        <button disabled={!isNameValid}>Отправить</button>
       </div>
     </form>
   );
