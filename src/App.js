8d23e289c87d833ca6c9371da4b335edc95a25a6
Danil Shipy4ka <105577586+Shipy4kaRU@users.noreply.github.com>
2024-11-02 17:23:06 +0400

    add build version

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import SomeForm from "./components/SomeForm";
+import SomeInput from "./components/SomeInput";
 
 function App() {
   return (
     <div className="app">
-      <SomeForm />
+      <SomeInput />
     </div>
   );
 }
